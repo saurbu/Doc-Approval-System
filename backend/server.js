06f2e9da1d53cf2c3ba6fb608cecafc186ab02e3
iamsondev <sondipkumarsk@gmail.com>
2026-08-01 16:59:27 +0600

    feat: configure Cloudinary for file uploads

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import cors from "cors";
 import connectDb from './config/db.js'
 import seedAdmin from './utils/seedAdmin.js'
 import adminRoutes from "./routes/adminRoutes.js";
+import employeeRoutes from "./routes/employeeRoutes.js"
 const app = express()
 app.use(express.json())
 app.use(cors({
@@ -14,16 +15,17 @@ app.use(cors({
     credentials: true
 }));
 app.use("/api/admin", adminRoutes);
+app.use("/api/employee", employeeRoutes);
 connectDb()
-.then( async () => {
-    await seedAdmin() 
+    .then(async () => {
+        await seedAdmin()
 
-    const PORT = process.env.PORT || 3000
-    app.listen(PORT, ()=>{
-        console.log(`server is running on port ${PORT}`);
-        
+        const PORT = process.env.PORT || 3000
+        app.listen(PORT, () => {
+            console.log(`server is running on port ${PORT}`);
+
+        })
     })
-})
 
 
 
